259d90ef489c72b4812f885766a2298a0bf28753
mmolalekan <muhayminolalekan@yahoo.com>
2024-03-07 10:32:47 +0100

    interview

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-
-
 '''Implementing Pascal's triangle.
 '''
+
 
 def pascal_triangle(n):
     '''returns a Pascal's triangle of a given integer.
     '''
-   
     if type(n) is not int or n <= 0:
-         return []
+        return []
     res = [[1]]
-    for i in range(n -1):
+    for i in range(n - 1):
         temp = [0] + res[-1] + [0]
         row = []
         for j in range(len(res[-1]) + 1):
-           
-            row.append(temp[j] +temp[j +1])
+            row.append(temp[j] + temp[j + 1])
         res.append(row)
     return res
