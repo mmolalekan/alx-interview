9b624fb4edd24c00a1419b439cf2ae513afbce1e
mmolalekan <muhayminolalekan@yahoo.com>
2024-03-22 10:02:36 +0100

    another implementation of pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,27 @@
-#!/usr/bin/python3
-'''Implementing Pascal's triangle.
-'''
+#!/bin/python3
+"""implementation of a pascal triangle"""
+
+
+def factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        result = n * factorial(n-1)
+    return result
 
 
 def pascal_triangle(n):
-    '''returns a Pascal's triangle of a given integer.
-    '''
-    if type(n) is not int or n <= 0:
+    """
+    returns a list of lists of integers representing the Pascal’s triangle of n
+    Returns an empty list if n <= 0
+    """
+    if n <= 0:
         return []
-    res = [[1]]
-    for i in range(n - 1):
-        temp = [0] + res[-1] + [0]
-        row = []
-        for j in range(len(res[-1]) + 1):
-            row.append(temp[j] + temp[j + 1])
-        res.append(row)
-    return res
+    result = []
+    for i in range(n):
+        temp = []
+        for k in range(i + 1):
+            res = (factorial(i)) / (factorial(k) * factorial(i - k))
+            temp.append(round(res))
+        result.append(temp)
+    return result
