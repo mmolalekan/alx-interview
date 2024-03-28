756559ce5a5fe076594cb5b9728a0fa5a04f8071
mmolalekan <muhayminolalekan@yahoo.com>
2024-03-28 18:09:24 +0100

    0x02

---
+++ interview_notes.py
@@ -0,0 +1,13 @@
+"""
+Project: pascal triangles
+Resource: Algorithms in python (Youtube video)
+Topics:
+  1. Simple Recursive Algorithms
+  2. Algorithms within Data Structures
+  3. Divide and conquer
+  4. Greedy Algorithms
+  5. Dynamic Programming
+
+  Simple recursive algorithms(too long, watch later)
+  
+"""
